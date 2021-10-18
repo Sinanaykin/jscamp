6b4080ea4dba6efcf0c05857d0d3da1015cb2f3d
Sinanaykin <sinanaykinnn@gmail.com>
2021-10-19 00:04:40 +0300

    Javascript başlangıç

---
+++ src/app.js
@@ -0,0 +1,27 @@
+console.log("Merhaba Dünya")
+
+let dolarBugun=9.30
+var dolarDun=9.20
+
+{
+    var dolarDun=9.10
+}
+
+
+console.log(dolarDun)
+
+const euroDun=11.2
+//euroDun=11
+console.log(euroDun)
+//-------------------------------------------------------------
+//array
+
+let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>" +konutKredileri[i]+"</li>");
+    
+}
+console.log("</ul>")
+console.log(konutKredileri)
